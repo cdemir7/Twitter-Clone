16f492f6d836fe1111658d582357db86f100cd7a
Cihan DEMİR <56312037+cdemir7@users.noreply.github.com>
2024-03-24 03:40:33 +0300

    Divide the home page into three sections

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,18 @@ import './App.css';
 import HomePage from './Componenets/HomePage/HomePage';
 import Authentication from './Componenets/Authentication/Authentication';
 
+
 function App() {
   return (
     <div className="">
       
+    
       <Routes>
         <Route path="/" element={true?<HomePage/>:<Authentication/>}>
 
         </Route>
       </Routes>
-
+    
     </div>
   );
 }
--- frontend/src/Componenets/HomePage/HomePage.jsx
+++ frontend/src/Componenets/HomePage/HomePage.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Grid } from '@mui/material';
+import React from 'react';
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <Grid container xs={12} className='px-5 lg:px-36 justify-between'>
 
-export default HomePage
+      <Grid item xs={0}  lg={2.5} className='hidden lg:block w-full relative'>
+        <p className='text-center'>Left Part</p>
+      </Grid>
+
+      <Grid item xs={12} lg={6} className='hidden lg:block w-full relative'>
+        <p className='text-center'>Middle Part</p>
+      </Grid>
+
+      <Grid item xs={0} lg={3} className='hidden lg:block w-full relative'>
+        <p className='text-center'>Right Part</p>
+      </Grid>
+
+    </Grid>
+  );
+};
+
+export default HomePage;
