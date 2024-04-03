aaafd29abeed5ab924ccf17b00d239beb9883a09
Cihan DEMİR <56312037+cdemir7@users.noreply.github.com>
2024-04-04 02:58:23 +0300

    Create a backend section of the project

---
+++ frontend/src/Componenets/Navigation/NavigationMenu.js
@@ -0,0 +1,11 @@
+import HomeIcon from "@mui/icons-material/Home"
+import ExploreIcon from "@mui/icons-material/Explore"
+import NotificationIcon from "@mui/icons-material/Notifications"
+import MessageIcon from '@mui/icons-material/Message';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+export const navigation=[
+    {
+        title:"Home", 
+        icon:<HomeIcon/>
+    }
+]
